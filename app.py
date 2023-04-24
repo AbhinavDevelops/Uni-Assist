f9d809ab030ba1656dc44f30df8a28ab15a5e0f2
Wei <wac.weichong@gmail.com>
2023-04-24 16:02:34 +0800

    debugging routing system

--- app.py
+++ app.py
@@ -72,19 +72,19 @@ def homepage():
     return render_template("homepage.html")
 
 @app.route("/discussion")
-def homepage():
+def discussion():
     return render_template("discussion.html")
 
 @app.route("/pricing")
-def homepage():
+def pricing():
     return render_template("pricing.html")
 
 @app.route("/contact")
-def homepage():
+def contact():
     return render_template("contact.html")
 
 @app.route("/ai-chat")
-def homepage():
+def aiChat():
     return render_template("index.html")
 
 if __name__ == '__main__':
