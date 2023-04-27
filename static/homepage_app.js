3a2c43bb32ff52e0ef2bef694478b23c11256c47
Wei <wac.weichong@gmail.com>
2023-04-27 19:13:07 +0800

    contact form response added: 'thanks for reaching out, [name], etc'

--- static/homepage_app.js
+++ static/homepage_app.js
@@ -13,4 +13,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const hiddenImages = document.querySelectorAll(".hidden");
     hiddenImages.forEach((element) => intObserver.observe(element))
+
+    const contactSubmit = document.querySelector("#contact-submit-button");
+    const contactDiv = document.querySelector("#contact-form-container");
+    const nameInput = document.querySelector("#name");
+
+    contactSubmit.addEventListener("click", (e) => {
+        e.preventDefault();
+        console.log("triggered");
+        const submissionConfirmation = document.createElement("h5");
+        submissionConfirmation.classList.add("submission-confirm")
+        const name = nameInput.value;
+        submissionConfirmation.innerHTML= `Thank you for reaching out, <span>${name}!</span> We will get back to you by email within 3 business days!`;
+        contactDiv.appendChild(submissionConfirmation);
+    })
 })
+
