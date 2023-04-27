6221f6a883aa3cf2cde8a1584801dcb112260ab7
Wei <wac.weichong@gmail.com>
2023-04-27 18:46:46 +0800

    features with animations added, new favicon created and assigned.

--- static/homepage_app.js
+++ static/homepage_app.js
@@ -1,3 +1,16 @@
-$(document).ready(function(){
-    $('.carousel').carousel();
-});
+document.addEventListener("DOMContentLoaded", () => {
+    const intObserver = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                console.log("Intersecting")
+                entry.target.classList.add("show");
+            }
+            else {
+                entry.target.classList.remove("show");
+            }
+        })
+    })
+
+    const hiddenImages = document.querySelectorAll(".hidden");
+    hiddenImages.forEach((element) => intObserver.observe(element))
+})
