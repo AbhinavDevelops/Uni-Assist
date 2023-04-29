811ee225a82a26642a3457b39f968291e622c49d
Wei <wac.weichong@gmail.com>
2023-04-29 10:46:41 +0800

    added typing animation for AI, displays user question immediately, and response when done, and cursor now changes to pointer when hovering UWA logo

--- static/gpt.js
+++ static/gpt.js
@@ -1,120 +1,125 @@
-
-
-
 document.addEventListener('DOMContentLoaded', function() {
 
-  // This line declares a constant variable named "apiKey" and assigns it a string value
-const apiKey = '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';
+    const typingIndicator = document.querySelector(".typing-indicator");
 
-// This array will store the history of questions and responses
-let history = [];
-
-  // Get the input and send button elements
-  const input = document.getElementById('question');
-  const sendButton = document.getElementById("chatbot-body-input-button");
-
-  // Listen for the 'keydown' event on the input element
-  input.addEventListener('keydown', function(event) {
-    // Check if the 'enter' key was pressed
-    if (event.key === 'Enter') {
-      // Do something, like submit a form or call a function
-      get_response();
-      input.value = "";
+    async function showTypingIndicator() {
+        typingIndicator.style.display = 'block';
+        console.log("Show");
     }
 
-    sendButton.addEventListener("click", get_response)
-
-  });
-
-// This function sends a request to the OpenAI API to generate a response based on the user's input
-function get_response() {
-    // This line gets the value of the input field with the ID "question"
-    var question = document.getElementById("question").value;
-
-    // This constant variable specifies which OpenAI model to use for generating the response
-    const model = 'text-davinci-003';
-
-    let chatContext = [];
-    for (let i = 0; i < history.length-1; i++) {
-        chatContext.push(history[i].response);
+    async function hideTypingIndicator() {
+        typingIndicator.style.display = 'none';
+        console.log("Hide");
     }
 
-    // This function sends a POST request to the OpenAI API with the specified parameters
-    fetch('https://api.openai.com/v1/completions', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiKey}`, // This line sets the API key for authentication
-      },
-      body: JSON.stringify({
-        model: model,
-        prompt: `${question}? This is our chat history, for context:\n${chatContext}, `,
-        temperature: 0.8,
-        max_tokens: 250,
-      }),
-    })
-    // This line parses the response from the API and extracts the generated text
-    .then(response => response.json())
-    .then(data => {
-        // This line stores the question and response in the history array
-        history.push({question: question, response: data.choices[0].text.trim()});
-        // This line updates the HTML to display the current response and the history of questions and responses
-        document.getElementById("history-list").innerHTML = "";
-        for (let i = 0; i < history.length; i++) {
-            // Select history display
-            const historyDisplay = document.querySelector("#history-list");
+    const apiKey = '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';
+    let history = [];
 
-            // Create new list item each for "User" (you), [your question], "AI Assistant", [its response]
+    const input = document.getElementById('question');
+    const sendButton = document.getElementById("chatbot-body-input-button");
+    const historyDisplay = document.querySelector("#history-list");
+
+    input.addEventListener('keydown', async function(event) {
+        if (event.key === 'Enter') {
+            showTypingIndicator();
             const newUserLabel = document.createElement("p");
             const newUserText = document.createElement("div");
-            const newResponseLabel = document.createElement("p");
-            const newResponseText = document.createElement("div");
 
-
-            // Format the static labels
             newUserLabel.innerText = "You";
             newUserLabel.style.color = "black";
             newUserLabel.style.fontWeight = "bold";
             newUserLabel.classList.add("question");
-            newResponseLabel.innerText = "AI Assistant";
-            newResponseLabel.style.color = "black";
-            newResponseLabel.style.fontWeight = "bold";
-            newResponseLabel.classList.add("response");
-
-            // Get the question and response data from history
             newUserText.classList.add("question");
-            newUserText.innerText = history[i].question;
-            newResponseText.classList.add("response");
+            newUserText.innerText = input.value;
 
-            let aiResponse = history[i].response;
-            //  Removes occasional random comma at the start of responses
-            if (aiResponse.charAt(0) === ",") {
-                aiResponse = aiResponse.slice(1);
-            }
-            // Response if API returns nothing - happens occasionally
-            if (aiResponse !== "") {
-                newResponseText.innerText = aiResponse;
-            }
-            else {
-                newResponseText.innerText = "Please try asking that again, or try a different question."
-            }
-
-            document.getElementById("response").innerHTML = aiResponse;
-
-            // Display the new list items
-            const newLis = [newUserLabel, newUserText, newResponseLabel, newResponseText];
+            const newLis = [newUserLabel, newUserText];
             for(let li of newLis) {
                 historyDisplay.appendChild(li);
             }
+
+            get_response(input.value);
+            input.value = "";
         }
-    })
-    .catch(error => console.error(error)); // This line logs any errors that occur during the request
-}
+
+        sendButton.addEventListener("click", function() {
+            showTypingIndicator();
+            const newUserLabel = document.createElement("p");
+            const newUserText = document.createElement("div");
+
+            newUserLabel.innerText = "You";
+            newUserLabel.style.color = "black";
+            newUserLabel.style.fontWeight = "bold";
+            newUserLabel.classList.add("question");
+            newUserText.classList.add("question");
+            newUserText.innerText = input.value;
+
+            const newLis = [newUserLabel, newUserText];
+            for(let li of newLis) {
+                historyDisplay.appendChild(li);
+            }
+
+            get_response(input.value);
+            input.value = "";
+        });
+    });
+
+    function get_response(question) {
+
+        const model = 'text-davinci-003';
+
+        let chatContext = [];
+        for (let i = 0; i < history.length-1; i++) {
+            chatContext.push(history[i].response);
+        }
+
+        fetch('https://api.openai.com/v1/completions', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${apiKey}`,
+            },
+            body: JSON.stringify({
+                model: model,
+                prompt: `This is our chat history, for context:"\n\n${chatContext}\n\n ${question}?"`,
+                temperature: 0.6,
+                max_tokens: 250,
+            }),
+        })
+            .then(response => response.json())
+            .then(data => {
+                const newResponseLabel = document.createElement("p");
+                const newResponseText = document.createElement("div");
+
+                newResponseLabel.innerText = "AI Assistant";
+                newResponseLabel.style.color = "black";
+                newResponseLabel.style.fontWeight = "bold";
+                newResponseLabel.classList.add("response");
+                newResponseText.classList.add("response");
+
+                let aiResponse = data.choices[0].text.trim();
+                if (aiResponse.charAt(0) === ",") {
+                    aiResponse = aiResponse.slice(1);
+                }
+                if (aiResponse !== "") {
+                    newResponseText.innerText = aiResponse;
+                }
+                else {
+                    newResponseText.innerText = "Please try asking that again, or try a different question."
+                }
+
+                const newLis = [newResponseLabel, newResponseText];
+                for(let li of newLis) {
+                    historyDisplay.appendChild(li);
+                }
+                hideTypingIndicator();
+                history.push({question: question, response: data.choices[0].text.trim()});
+            })
+            .catch(error => console.error(error));
+    }
+
 });
 
 function clear_history() {
-  history = [];
-  document.getElementById("history-list").innerHTML = "";
+    history = [];
+    document.getElementById("history-list").innerHTML = "";
 }
-
-
