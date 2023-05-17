28421e416f61dd22ebbfb4cdee02870289ad350e
Abhinav Rajaram <abhin2@MacBook-Air.local>
2023-05-17 14:18:09 +0800

    if no image is uploaded default picture is used

--- app.py
+++ app.py
@@ -61,6 +61,8 @@ def login():
 
         if user:
             session['username'] = user[1]
+            if user[3] == None or user[3] == "" or os.path.isfile(user[3]) == False:
+                session['profile_pic_path'] = 'static'+'/pfp/'+'default.png'
             session['profile_pic_path'] = user[3]
             print("one")
             return redirect(url_for('homepage'))
@@ -78,7 +80,8 @@ def register():
         password = request.form['password']
         profile_pic = request.files['profile_pic']
         profile_pic_path = 'static'+'/pfp/'+profile_pic.filename
-        profile_pic.save(profile_pic_path)
+        if profile_pic.filename != "":
+            profile_pic.save(profile_pic_path)
         # Check if username already exists in the database
         conn = sqlite3.connect('users.db')
         c = conn.cursor()
